9c0fc61b6f01427b85889870a5e95560fc8cc75d
HH-v <moliqingchaplus@gmail.com>
2026-05-23 22:48:51 +0800

    feat: initialize package structure with exports

---
+++ parallel_dl/__init__.py
@@ -0,0 +1,6 @@
+from .downloader import Downloader
+from .chunk import ChunkManager
+from .progress import ProgressTracker
+
+__version__ = "0.1.0"
+__all__ = ["Downloader", "ChunkManager", "ProgressTracker"]
