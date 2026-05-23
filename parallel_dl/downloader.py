095599db469c8b31dd58ba164eb2d12cfee96358
HH-v <moliqingchaplus@gmail.com>
2026-05-23 22:48:57 +0800

    feat: add file size detection via HEAD request

--- parallel_dl/downloader.py
+++ parallel_dl/downloader.py
@@ -1,6 +1,10 @@
 import os
 import threading
+import logging
 from concurrent.futures import ThreadPoolExecutor, as_completed
+from .chunk import ChunkManager
+
+logger = logging.getLogger(__name__)
 
 class Downloader:
     def __init__(self, url, dest=None, threads=4, chunk_size=1024*1024):
@@ -8,12 +12,18 @@ class Downloader:
         self.dest = dest or os.path.basename(url)
         self.threads = threads
         self.chunk_size = chunk_size
-        self._executor = None
     
     def start(self):
-        self._executor = ThreadPoolExecutor(max_workers=self.threads)
-        return self._executor
+        logger.info(f"Starting download: {self.url}")
+        return ThreadPoolExecutor(max_workers=self.threads)
+    
+    def get_file_size(self):
+        import requests
+        resp = requests.head(self.url, allow_redirects=True)
+        size = int(resp.headers.get("content-length", 0))
+        if size == 0:
+            raise ValueError("Server did not provide content-length")
+        return size
     
     def shutdown(self):
-        if self._executor:
-            self._executor.shutdown(wait=True)
+        pass
