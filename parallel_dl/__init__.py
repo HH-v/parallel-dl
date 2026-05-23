0593dbfe186344354a4f3a600842380b65027119
HH-v <moliqingchaplus@gmail.com>
2026-05-23 22:49:03 +0800

    feat: export CLI main, bump version to 0.2.0

--- parallel_dl/__init__.py
+++ parallel_dl/__init__.py
@@ -1,6 +1,8 @@
 from .downloader import Downloader
 from .chunk import ChunkManager
 from .progress import ProgressTracker
+from .retry import retry, ResumeInfo
+from .cli import main
 
-__version__ = "0.1.0"
-__all__ = ["Downloader", "ChunkManager", "ProgressTracker"]
+__version__ = "0.2.0"
+__all__ = ["Downloader", "ChunkManager", "ProgressTracker", "retry", "ResumeInfo", "main"]
